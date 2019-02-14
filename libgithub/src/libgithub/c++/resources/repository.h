13e30bb7486eae2c1f708abb39fb3f3549bfb48a
Crisostomo, Enrico M <enrico.m.crisostomo@accenture.com>
2019-02-14 17:56:05 +0100

    Add repository fields

--- libgithub/src/libgithub/c++/resources/repository.h
+++ libgithub/src/libgithub/c++/resources/repository.h
@@ -44,6 +44,83 @@ namespace github
     std::vector<topic> get_topics();
     void set_topics(std::vector<topic> topics);
     void transfer(std::string user, std::vector<unsigned int> team_ids);
+
+    uint64_t id;
+    std::string node_id;
+    std::string name;
+    std::string full_name;
+    // TODO: model the owner entity
+    // github::owner
+    bool private_repo;
+    std::string description;
+    bool fork;
+    std::string url;
+    std::string archive_url;
+    std::string assignees_url;
+    std::string blobs_url;
+    std::string branches_url;
+    std::string collaborators_url;
+    std::string comments_url;
+    std::string commits_url;
+    std::string compare_url;
+    std::string contents_url;
+    std::string contributors_url;
+    std::string deployments_url;
+    std::string downloads_url;
+    std::string events_url;
+    std::string forks_url;
+    std::string git_commits_url;
+    std::string git_refs_url;
+    std::string git_tags_url;
+    std::string git_url;
+    std::string issue_comment_url;
+    std::string issue_events_url;
+    std::string issues_url;
+    std::string keys_url;
+    std::string labels_url;
+    std::string languages_url;
+    std::string merges_url;
+    std::string milestones_url;
+    std::string notifications_url;
+    std::string pulls_url;
+    std::string releases_url;
+    std::string ssh_url;
+    std::string stargazers_url;
+    std::string statuses_url;
+    std::string subscribers_url;
+    std::string subscription_url;
+    std::string tags_url;
+    std::string teams_url;
+    std::string trees_url;
+    std::string clone_url;
+    std::string mirror_url;
+    std::string hooks_url;
+    std::string svn_url;
+    std::string homepage;
+    std::string repo_language;
+    uint64_t forks_count;
+    uint64_t stargazers_count;
+    uint64_t watchers_count;
+    uint64_t size;
+    std::string default_branch;
+    uint64_t open_issues_count;
+    std::vector<std::string> topics;
+    bool has_issues;
+    bool has_projects;
+    bool has_wiki;
+    bool has_pages;
+    bool has_downloads;
+    bool archived;
+    // TODO: refactor to dates
+    std::string pushed_at;
+    std::string created_at;
+    std::string updated_at;
+    // TODO: model permissions
+    // std::vector<permission> permissions;
+    uint64_t subscribers_count;
+    uint64_t network_count;
+    // TODO: model license
+    // github::license license;
   };
 };
 
